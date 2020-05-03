1fe21867a2119771e1e4335c02f028b270b63de6
chrisGaye <gayetalib90@gmail.com>
2020-05-03 21:23:45 +0000

    Validation Form

--- lib/MessageCompose.dart
+++ lib/MessageCompose.dart
@@ -11,31 +11,43 @@ class _MessageComposeState extends State<MessageCompose>{
    String subject  = "";
    String body  = "";
 
+   final key = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Compose New Message')
-      ),
-      body: SingleChildScrollView(
-        child: Form(
-        //  key: key,
-         child: Column(
-         // padding: EdgeInsets.all(16),
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            ListTile(
-               title: TextFormField(
-               onSaved: (value) => to = value,
-              decoration: InputDecoration(
-                  prefixIcon: Icon(Icons.add),
-                  labelText: "To",
-                  labelStyle: TextStyle(fontWeight: FontWeight.bold)
-              ),  
-            ),
-            ), 
-            ListTile(
-               title:  TextFormField(
+                return Scaffold(
+                  appBar: AppBar(
+                    title: Text('Compose New Message')
+                  ),
+                  body: SingleChildScrollView(
+                    child: Form(
+                    key: key,
+                 child: Column(
+                 // padding: EdgeInsets.all(16),
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    ListTile(
+                       title: TextFormField(
+                       validator: (value) => !value.contains('@') ? "Veuillez saisir un e-mail" : null,
+                       onSaved: (value) => to = value,
+                       decoration: InputDecoration(
+                          prefixIcon: Icon(Icons.add),
+                          labelText: "To",
+                          labelStyle: TextStyle(fontWeight: FontWeight.bold)
+                      ),  
+                    ),
+                    ), 
+                    ListTile(
+                       title:  TextFormField(
+                      validator: (value) {
+                        int len = value.length;
+                          if (len == 0) {
+                            return "Suject ne peut pas etre null";
+                          }else if(len < 4){
+                            return "Il faut au moins 4 caractères pour Subject";
+                          }
+                          return null;
+                        },
                onSaved: (value) => subject = value,
               decoration: InputDecoration(
                   labelText: "Subject",
@@ -59,9 +71,12 @@ class _MessageComposeState extends State<MessageCompose>{
                title: RaisedButton(
                  child: Text("SEND"),
                  onPressed: () {
+                   if(this.key.currentState.validate()){
+                      this.key.currentState.save();
+                   }
                   //  Message message = Message (subject, body);
-                  // this.key.currentState.save();
-                  Navigator.pop(context, "GAYE");
+                  
+                  // Navigator.pop(context, "Ca marche");
                }),
             ),
           ],
