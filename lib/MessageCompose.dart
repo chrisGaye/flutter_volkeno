725d6ef0c046f874b8e707867a658d919c1d66c8
chrisGaye <gayetalib90@gmail.com>
2020-05-03 16:23:34 +0000

    form part_1

--- lib/MessageCompose.dart
+++ lib/MessageCompose.dart
@@ -1,27 +1,73 @@
 import 'package:flutter/material.dart';
 
-class MessageCompose extends StatelessWidget {
+class MessageCompose extends StatefulWidget {
+  @override
+  _MessageComposeState createState() => _MessageComposeState();
+}
+
+class _MessageComposeState extends State<MessageCompose>{
+  //Definir les variables
+   String to  = "";
+   String subject  = "";
+   String body  = "";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Compose New Message')
       ),
-      body: Container(
-        padding: EdgeInsets.all(16),
-        child: Column(
+      body: SingleChildScrollView(
+        child: Form(
+        //  key: key,
+         child: Column(
+         // padding: EdgeInsets.all(16),
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            TextField(), TextField(), 
-            TextField(
+            ListTile(
+               title: TextFormField(
+               onSaved: (value) => to = value,
               decoration: InputDecoration(
-                labelText: 'BODY', labelStyle: TextStyle()
+                  prefixIcon: Icon(Icons.add),
+                  labelText: "To",
+                  labelStyle: TextStyle(fontWeight: FontWeight.bold)
+              ),  
+            ),
+            ), 
+            ListTile(
+               title:  TextFormField(
+               onSaved: (value) => subject = value,
+              decoration: InputDecoration(
+                  labelText: "Subject",
+                  labelStyle: TextStyle(fontWeight: FontWeight.bold)
+              ),  
+             ),
+            ),
+           
+            Divider(),
+            ListTile(
+              title: TextFormField(
+              onSaved: (value) => body = value,
+              decoration: InputDecoration(
+                labelText: 'BODY', 
+                labelStyle: TextStyle(fontWeight: FontWeight.bold)
               ),
               maxLines: 5,
-            )
+             ),
+            ),
+            ListTile(
+               title: RaisedButton(
+                 child: Text("SEND"),
+                 onPressed: () {
+                  //  Message message = Message (subject, body);
+                  // this.key.currentState.save();
+                  Navigator.pop(context, "GAYE");
+               }),
+            ),
           ],
-        ),
-      ),
+         ),
+       ),
+      )
     );
   }
 }
